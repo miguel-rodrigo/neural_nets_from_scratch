e54308cb3cef877ea31d262c79e0b8b8bdce9370
RODRIM7K <miguel_angel.rodrigo_lisbona@novartis.com>
2020-08-08 16:51:59 +0200

    Add printing to the model for quick summary of shapes, units...

--- Model.py
+++ Model.py
@@ -5,9 +5,37 @@ import Activation
 
 class Model:
     def __init__(self, n_units_vector, activations):
+        self.input_size = n_units_vector[0]
+        self.layer_sizes = n_units_vector[1:]
+
         # Combine n_units of the next with n_units of the prev layer:
         dim_vector = zip(n_units_vector[:-1], n_units_vector[1:])
         layer_info = zip(dim_vector, activations)
 
         self.layers = [Layer(units_prev, units_next, activation) for
                        (units_prev, units_next), activation in layer_info]
+
+    def __str__(self):
+        s = ""
+
+        # General summary:
+        s += "Input dimension: " + str(self.input_size) + "\n"
+        s += "Output dimension: " + str(self.layer_sizes[-1]) + "\n"
+
+        # Layer by layer:
+        for i, layer in enumerate(self.layers):
+            s += "\n"
+            s += "----------\n"
+            s += "Layer " + str(i+1) + ":\n"
+            s += "----------\n"
+            s += "# of units: " + str(self.layer_sizes[i]) + "\n"
+            s += "  - W: " + str(layer.W.shape) + "\n"
+            s += "  - b: " + str(layer.b.shape) + "\n"
+
+        return s
+
+
+if __name__ == "__main__":
+    n_units = [4, 4, 2, 1]
+    activations = [Activation.ReLU, Activation.ReLU, Activation.Sigmoid]
+    model = Model(n_units, activations)
