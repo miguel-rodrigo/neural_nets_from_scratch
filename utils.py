fdb968e061558a23b58e07a99f730982cf6d8d80
Mike Desktop PC <miguelrodrigo249@gmail.com>
2020-08-13 22:43:05 +0100

    Create function to draw model decision boundary

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def create_fake_data(num_points=200, num_petals=8, random_jitter_strength=0.05):
@@ -30,5 +31,18 @@ def create_train_test_split(train_to_test_ratio=0.8):
     pass
 
 
-def draw_decision_boundary(model):
-    pass
+def draw_decision_boundary(model, X, y):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
+    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
