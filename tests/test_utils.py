351a153d60c893ea3196aaec9b7bca156750e5a8
Mike Desktop PC <miguelrodrigo249@gmail.com>
2020-08-16 20:34:09 +0100

    Create skeleton for testing utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,23 +1,14 @@
-# import numpy as np
-#
-# import utils
-# import activations
-# from model import Model
-#
-#
-# class TestUtils:
-#     def setup(self):
-#         np.random.seed(1)
-#         n_units = [2, 4, 1]
-#
-#         activations = [activations.ReLU, activations.Sigmoid]
-#         self.model = Model(n_units, activations)
-#
-#         X, Y = utils.create_fake_data()
-#         n_epochs = 1000
-#         for i, iteration_loss in enumerate(self.model.train(X, Y, n_epochs=n_epochs)):
-#             if i % 100 == 0 or i == n_epochs - 1:
-#                 print("Loss on iteration {}: {}".format(i, iteration_loss))
-#
-#     def test_gradient_checking(self):
-#         self.assertEqual(True, False)
+import numpy as np
+
+
+class TestUtils:
+    def setup(self):
+        """
+        Create a fake model object, which has all that is needed (parameters, gradients and cost function) but
+        everything has been manually calculated on paper.
+
+        Then try a case when the gradients are correct, and a case when they are not.
+        """
+
+    def test_gradient_checking(self):
+        np.testing.assert_equal(True, False)
